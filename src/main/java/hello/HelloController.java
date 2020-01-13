4875a08b08bed9020349690537c0cec0007d3821
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2020-01-13 13:43:30 -0800

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    //@RequestMapping("/")
+    //public String index() {
+        //return "Greetings from Spring Boot!";
+    //}
+    
+    String html = "<h1>Hello World!</h1>\n" +
+        "<p>This web app is powered by \n" +
+        "<a href='https://github.com/ucsb-cs56-w20/lab02-oliviaclough'>this github repo</a></p>\n";
+    return html;
     
 }
